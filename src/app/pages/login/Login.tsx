4a12da5a234f8b72fa5f60640e8e7c0a5feb61b8
Amadeu dos Anjos Barros <anjosbarrosclean@gmail.com>
2023-06-06 20:58:04 -0300

    adicionando input a tela de login

--- src/app/pages/login/Login.tsx
+++ src/app/pages/login/Login.tsx
@@ -1,3 +1,48 @@
+import { useState } from "react";
+
+export const Login = () => {
+
+    const [email, setEmail] = useState('teste');
+    const [pass, setPass] = useState('');
+
+    const handleEntrar = () => {
+        console.log(email)
+        console.log(pass)
+    }
+
+    return(
+        <div>
+            <h3>Login</h3>
+            <form action="">
+                <label>
+                    <span>Email </span>
+                    <input value={email} onChange={e => setEmail(e.target.value)}/>
+                </label>
+                <br />
+                <br />
+                <label>
+                    <span>Senha </span>
+                    <input type="password" value={pass} onChange={e => setPass(e.target.value)}/>
+                </label>
+                <br />
+                <br />
+                <button type="button" onClick={handleEntrar}>Entrar</button>
+            </form>
+        </div>
+    );
+}
+
+
+
+
+
+
+
+
+
+
+
+/* //Maneira de navegar entre páginas sem ter que carregar. Essa é através de função.
 import { useNavigate } from "react-router-dom";
 
 export const Login = () => {
@@ -15,4 +60,4 @@ export const Login = () => {
     );
 }
 
-//Maneira de navegar entre páginas sem ter que carregar. Essa é através de função.
+ */
