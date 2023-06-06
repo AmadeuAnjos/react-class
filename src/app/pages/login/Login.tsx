051129f27925f3f3dac5323a2fe257ded2249841
Amadeu dos Anjos Barros <anjosbarrosclean@gmail.com>
2023-06-05 21:42:48 -0300

    adicionando diretorio login ao projeto

---
+++ src/app/pages/login/Login.tsx
@@ -0,0 +1,11 @@
+import { Link } from 'react-router-dom'
+
+export const Login = () => {
+    return (
+        <div>
+            <h1>Login</h1>
+            <Link to="/dashboard">Dash</Link>
+        </div>
+    );
+}
+
